771f6645cf35976e38113094e186d2505781eb88
Sedat <34796337+sedatbozdogan@users.noreply.github.com>
2018-08-25 00:36:52 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/BaseClass.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp1
+{
+    public class BaseClass
+    {
+        public virtual int publicUye { get; set; }
+        protected virtual int protectedUye { get; set; }
+        internal virtual int internalUye { get; set; }
+        protected virtual internal int protectedInternalUye { get; set; }
+        private int privateUye { get; set; }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass TemelSinif = new BaseClass();
+            //TemelSinif.
+        }
+    }
+}
